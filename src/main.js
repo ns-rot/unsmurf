e7a60f9b8cc6190d173c95eaf08f6ff74dc3d94f
Edward <rotenaple@protonmail.com>
2025-01-25 06:19:44 +1030

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import App from './App.svelte';
 
-const production = process.env.NODE_ENV === 'production';
-const basePath = production ? '/unsmurf/' : '/';
-
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world',
-		basePath, // Pass the base path as a prop
+		name: 'world'
 	}
 });
 
